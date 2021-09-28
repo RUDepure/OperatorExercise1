64a1e89ce5fa45313904306613d266e2d12f3b92
RUDepure <alexrum92@gmail.com>
2021-09-28 12:03:53 -0500

    Added Exercise 2 solution

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -6,11 +6,20 @@ namespace OperatorExercise
 {
     class Program
     {
+        //Operator Exercise 2
+        public static double AreaOfCicle(double r)
+        {
+            double result;
+
+            result = (Math.PI * (r * r));
+
+            return result;
+        }
         static void Main(string[] args)
         {
+            //Operator Exercise 1
             int a = 17;
             int b = 4;
-
             int quotient = a / b;
             int remainder = a % b;
 
@@ -18,6 +27,13 @@ namespace OperatorExercise
             {
                 Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
             }
+
+            //Operator Exercise 2
+            double area;
+            Console.WriteLine("What is the radius of your circle ?");
+            var radius = double.Parse(Console.ReadLine());
+            area = AreaOfCicle(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
         }
     }
 }
